828334f5d882901aea43fa58fdc33a28c27a258e
MichaAI <michaai@neodustry.ru>
2025-04-19 23:06:34 +0300

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,25 @@
+#![no_std]
+#![no_main]
+
+use core::panic::PanicInfo;
+
+const HELLO: &[u8] = b"Hello World";
+
+#[unsafe(no_mangle)]
+pub extern "C" fn _start() -> ! {
+    let vga_buff = 0xb8000 as *mut u8;
+
+    for (i, &byte) in HELLO.iter().enumerate() {
+        unsafe {
+            *vga_buff.offset(i as isize * 2) = byte;
+            *vga_buff.offset(i as isize * 2 + 1) = 0xb;
+        }
+        
+    }
+    loop {}
+}
+
+#[panic_handler]
+fn panic_handler(_info: &PanicInfo) -> ! {
+    loop {}
+}
